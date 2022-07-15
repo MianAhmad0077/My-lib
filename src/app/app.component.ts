c65a4b8423e33b32d410203131cebf0aaf2c5bb1
unknown <you@example.com>
2022-07-15 15:17:00 +0500

    edit custom library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedLibrary } from './common/custom_library/date-range-validator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My-lib';
+
+  constructor(
+   private sharedLibrary: SharedLibrary
+  ){}
+
+  myFunc(): void {
+ this.sharedLibrary.sharedFunc();
+  }
+
 }
---
+++ src/app/common/custom_library/date-range-validator.ts
@@ -0,0 +1,8 @@
+import { range } from "rxjs"
+
+export class SharedLibrary {
+
+    sharedFunc(): void {
+
+    }
+}
